c31218bb69cab13f244fb9b332b1d944afe8b88c
kongr45gpen <electrovesta@gmail.com>
2021-06-03 02:20:08 +0300

    Add ECSS TM structure

--- src/main/java/com/example/myproject/MyPacketPreprocessor.java
+++ src/main/java/com/example/myproject/MyPacketPreprocessor.java
@@ -16,7 +16,7 @@ import org.yamcs.utils.TimeEncoding;
  * A single instance of this class is created, scoped to the link udp-in.
  * <p>
  * This is specified in the configuration file yamcs.myproject.yaml:
- * 
+ *
  * <pre>
  * ...
  * dataLinks:
@@ -32,6 +32,7 @@ import org.yamcs.utils.TimeEncoding;
 public class MyPacketPreprocessor extends AbstractPacketPreprocessor {
 
     private Map<Integer, AtomicInteger> seqCounts = new HashMap<>();
+    int counter = 0;
 
     // Constructor used when this preprocessor is used without YAML configuration
     public MyPacketPreprocessor(String yamcsInstance) {
@@ -48,25 +49,25 @@ public class MyPacketPreprocessor extends AbstractPacketPreprocessor {
     public TmPacket process(TmPacket packet) {
 
         byte[] bytes = packet.getPacket();
-        if (bytes.length < 6) { // Expect at least the length of CCSDS primary header
+        if (bytes.length < 5) { // Expect at least the length of CCSDS primary header
             eventProducer.sendWarning("SHORT_PACKET",
-                    "Short packet received, length: " + bytes.length + "; minimum required length is 6 bytes.");
+                    "Short packet received, length: " + bytes.length + "; minimum required length is 5 bytes.");
 
             // If we return null, the packet is dropped.
             return null;
         }
 
         // Verify continuity for a given APID based on the CCSDS sequence counter
-        int apidseqcount = ByteBuffer.wrap(bytes).getInt(0);
-        int apid = (apidseqcount >> 16) & 0x07FF;
-        int seq = (apidseqcount) & 0x3FFF;
-        AtomicInteger ai = seqCounts.computeIfAbsent(apid, k -> new AtomicInteger());
-        int oldseq = ai.getAndSet(seq);
+        // int apidseqcount = ByteBuffer.wrap(bytes).getInt(0);
+        // int apid = (apidseqcount >> 16) & 0x07FF;
+        // int seq = (apidseqcount) & 0x3FFF;
+        // AtomicInteger ai = seqCounts.computeIfAbsent(apid, k -> new AtomicInteger());
+        // int oldseq = ai.getAndSet(seq);
 
-        if (((seq - oldseq) & 0x3FFF) != 1) {
-            eventProducer.sendWarning("SEQ_COUNT_JUMP",
-                    "Sequence count jump for APID: " + apid + " old seq: " + oldseq + " newseq: " + seq);
-        }
+        // if (((seq - oldseq) & 0x3FFF) != 1) {
+        //     eventProducer.sendWarning("SEQ_COUNT_JUMP",
+        //             "Sequence count jump for APID: " + apid + " old seq: " + oldseq + " newseq: " + seq);
+        // }
 
         // Our custom packets don't include a secundary header with time information.
         // Use Yamcs-local time instead.
@@ -74,7 +75,7 @@ public class MyPacketPreprocessor extends AbstractPacketPreprocessor {
 
         // Use the full 32-bits, so that both APID and the count are included.
         // Yamcs uses this attribute to uniquely identify the packet (together with the gentime)
-        packet.setSequenceCount(apidseqcount);
+        packet.setSequenceCount(counter++);
 
         return packet;
     }
