682cbde67f745427c466c5541d4a6b68dd85d298
kongr45gpen <electrovesta@gmail.com>
2021-06-10 16:03:30 +0300

    Properly handle serial errors without crashing

--- link/link.cpp
+++ link/link.cpp
@@ -40,55 +40,66 @@ int main() {
 
         std::function<void(const boost::system::error_code&,std::size_t)> udpHandler =
             [&udpHandler, &rxSocket, &rxEndpoint, &rxbuf, &serial](const boost::system::error_code& error, std::size_t bytes_transferred) {
-                std::array<unsigned char, 1024> cobs_buffer;
+                if (error.failed()) {
+                    std::cerr << error.category().name() << ": " << error.message() << std::endl;
+                    std::this_thread::sleep_for(500ms);
+                } else {
+                    std::array<unsigned char, 1024> cobs_buffer;
 
-                if (bytes_transferred > 4) {
-                    std::cout << "Received TC[" << int{rxbuf[1]} << "," << int{rxbuf[2]} << "] (" << bytes_transferred << " bytes)" << std::endl;
+                    if (bytes_transferred > 4) {
+                        std::cout << "Received TC[" << int{rxbuf[1]} << "," << int{rxbuf[2]} << "] (" << bytes_transferred << " bytes)" << std::endl;
+                    }
+
+                    // COBS encoding
+                    auto result = cobs_encode(cobs_buffer.data(), cobs_buffer.size() - 1, rxbuf.data(), bytes_transferred);
+
+                    if (result.status != COBS_ENCODE_OK) {
+                        std::cerr << "COBS encode status returned " << static_cast<int>(result.status) << std::endl;
+                    } else {
+                        // Send packet via serial
+                        cobs_buffer[result.out_len] = '\0';
+                        boost::asio::write(serial, boost::asio::buffer(cobs_buffer.data(), result.out_len + 1));
+                    }
                 }
 
                 rxSocket.async_receive_from(boost::asio::buffer(rxbuf), rxEndpoint, udpHandler);
-
-                // COBS encoding
-                auto result = cobs_encode(cobs_buffer.data(), cobs_buffer.size() - 1, rxbuf.data(), bytes_transferred);
-
-                if (result.status != COBS_ENCODE_OK) {
-                    std::cerr << "COBS encode status returned " << static_cast<int>(result.status) << std::endl;
-                } else {
-                    // Send packet via serial
-                    cobs_buffer[result.out_len] = '\0';
-                    boost::asio::write(serial, boost::asio::buffer(cobs_buffer.data(), result.out_len + 1));
-                }
         };
 
         rxSocket.async_receive_from(boost::asio::buffer(rxbuf), rxEndpoint, udpHandler);
 
         std::function<void(const boost::system::error_code&,std::size_t)> serialHandler =
             [&serial, &serialHandler, &buf, &txSocket, &endpoint](const boost::system::error_code& error, std::size_t bytes_transferred) -> void {
-            std::string receivedAll(reinterpret_cast<const char *>(buf.data().data()), buf.size());
 
-            // Find the first occurence of a zero
-            size_t zeroLocation = receivedAll.find('\0');
-            std::string receivedRaw(reinterpret_cast<const char *>(buf.data().data()), zeroLocation);
-            buf.consume(zeroLocation + 1);
-
-            // COBS decoding
-            std::vector<char> buffer(receivedRaw.size());
-            auto result = cobs_decode(buffer.data(), buffer.size(), receivedRaw.c_str(),
-                                            receivedRaw.size()); // strip the last byte
-
-            if (result.status != COBS_DECODE_OK) {
-                std::cerr << "COBS status returned " << static_cast<int>(result.status) << std::endl;
-            } else {
-                // Send correct packet via UDP
-
-                if (buffer[1] == -1 && buffer[2] == -1) {
-                    std::cout << std::string(buffer.data(), result.out_len).substr(4);
+                if (error.failed()) {
+                    std::cerr << error.category().name() << ": " << error.message() << std::endl;
+                    std::this_thread::sleep_for(500ms);
                 } else {
-                    txSocket.send_to(boost::asio::buffer(buffer.data(), result.out_len), endpoint);
-                }
-            }
+                    std::string receivedAll(reinterpret_cast<const char *>(buf.data().data()), buf.size());
 
-            // std::cout << "Received packet [" << receivedRaw.size() << "]" << std::endl;
+                    // Find the first occurence of a zero
+                    size_t zeroLocation = receivedAll.find('\0');
+                    std::string receivedRaw(reinterpret_cast<const char *>(buf.data().data()), zeroLocation);
+                    buf.consume(zeroLocation + 1);
+
+                    // COBS decoding
+                    std::vector<char> buffer(receivedRaw.size());
+                    auto result = cobs_decode(buffer.data(), buffer.size(), receivedRaw.c_str(),
+                                                    receivedRaw.size()); // strip the last byte
+
+                    if (result.status != COBS_DECODE_OK) {
+                        std::cerr << "COBS status returned " << static_cast<int>(result.status) << std::endl;
+                    } else {
+                        // Send correct packet via UDP
+
+                        if (buffer[1] == -1 && buffer[2] == -1) {
+                            std::cout << std::string(buffer.data(), result.out_len).substr(4);
+                        } else {
+                            txSocket.send_to(boost::asio::buffer(buffer.data(), result.out_len), endpoint);
+                        }
+                    }
+
+                    // std::cout << "Received packet [" << receivedRaw.size() << "]" << std::endl;
+                }
 
             // Read next data packet
             boost::asio::async_read_until(serial, buf, '\0', serialHandler);
@@ -99,38 +110,7 @@ int main() {
         io.run();
 
 
-    //     while (true) {
-    //         try {
-    //             boost::asio::read_until(serial, buf, '\0');
-    //             std::string receivedAll(reinterpret_cast<const char *>(buf.data().data()), buf.size());
-    //             // Find the first occurence of a zero
-    //             size_t zeroLocation = receivedAll.find('\0');
-    //             std::string receivedRaw(reinterpret_cast<const char *>(buf.data().data()), zeroLocation);
-    //             buf.consume(zeroLocation + 1);
 
-    //             // std::cout << "Received packet [" << receivedRaw.size() << "]" << std::endl;
-
-    //             // COBS decoding
-    //             std::vector<char> buffer(receivedRaw.size());
-    //             auto result = cobs_decode(buffer.data(), buffer.size(), receivedRaw.c_str(),
-    //                                           receivedRaw.size()); // strip the last byte
-
-    //             if (result.status != COBS_DECODE_OK) {
-    //                 std::cerr << "COBS status returned " << static_cast<int>(result.status) << std::endl;
-    //             } else {
-    //                 // Send correct packet via UDP
-
-    //                 if (buffer[1] == -1 && buffer[2] == -1) {
-    //                     std::cout << std::string(buffer.data(), result.out_len).substr(4);
-    //                 } else {
-    //                     // txSocket.send_to(boost::asio::buffer(buffer.data(), result.out_len), endpoint);
-    //                 }
-    //             }
-    //         } catch (boost::system::system_error &e) {
-    //             std::cerr << e.what() << std::endl;
-    //             std::this_thread::sleep_for(500ms);
-    //         }
-    //     }
     } catch (boost::system::system_error &e) {
         std::cerr << e.what() << std::endl;
     }
